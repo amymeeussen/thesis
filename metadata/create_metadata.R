7d5dbd0d210f4c561d77f5bd834aea0e667e4e4c
amyparsons <amyparsons@penguin>
2023-03-27 20:13:27 -0700

    sexed data at once

--- metadata/create_metadata.R
+++ metadata/create_metadata.R
@@ -1,7 +1,9 @@
+library(dplyr)
+library(ggplot2)
 
-field_data = read.csv("~/thesis/metadata/field_data.csv", na.strings = c("", "NA"))
-extraction_notes = read.csv("~/thesis/metadata/extraction_notes.csv", na.strings = c("", "NA"))
-argon_lab_data = read.csv("~/thesis/metadata/argon_lab_data.csv", na.strings = c("", "NA"))
+field_data = read.csv("~/thesis/metadata/field_data.csv")
+extraction_notes = read.csv("~/thesis/metadata/extraction_notes.csv")
+argon_lab_data = read.csv("~/thesis/metadata/argon_lab_data.csv")
 
 # Convert sample ID to bird id
 extraction_notes$Bird = ifelse(grepl("\\.", extraction_notes$Sample.ID),
@@ -12,5 +14,60 @@ extraction_notes$Bird = ifelse(grepl("\\.", extraction_notes$Sample.ID),
 all_data = merge(extraction_notes, field_data, by = "Bird", all.x = TRUE)
 all_data = merge(all_data, argon_lab_data, by = "Sample.ID", all.x = TRUE)
 
-# Write the end result to file
-write.table(all_data, file = "~/thesis/metadata/metadata.csv", sep = ",", row.names = FALSE, na = "")
+# Add column to record origin of sex data
+all_data$sex_origin = ifelse(all_data$sex == "", "", "Carly PCA")
+
+# Extract rows that have sex data
+sex_data = subset(all_data, select = c("TAR..mm.", "CUL..mm.", "SKULL..mm.", "MN..mm.", "MX..mm.", "Mass..g.", "sex", "Bird", "sex_origin"))
+print("Header of sex data:")
+colnames(sex_data)
+#sex_data = filter(sex_data, sex != "")
+sex_data = filter(sex_data, Mass..g. != "not taken")
+sex_data = filter(sex_data, MX..mm. != "NA")
+nrow(sex_data)
+
+# Plot sample two dimensional graph
+colors = ifelse(sex_data$sex == "F", "red",
+                ifelse(sex_data$sex == "M", "blue", "black"))
+plot(sex_data$TAR..mm., sex_data$CUL..mm., col=colors, pch=16)
+
+# Do PCA on the sex data
+dim_data = subset(sex_data, select = c("TAR..mm.", "CUL..mm.", "SKULL..mm.", "MN..mm.", "MX..mm.", "Mass..g."))
+dim_data = apply(dim_data, 2, as.numeric)
+pca_result <- prcomp(dim_data, scale=TRUE)
+dim_data_pca = data.frame(PC1 = pca_result$x[, 1], PC2 = pca_result$x[, 2])
+dim_data_pca$sex <- sex_data$sex
+dim_data_pca$Bird <- sex_data$Bird
+plot(dim_data_pca$PC1, dim_data_pca$PC2, col=colors, pch=16)
+
+# Apply clustering to all data, based on PC1: f < 0 and m > 1
+dim_data_pca$sex = ifelse(dim_data_pca$sex == "",
+                          ifelse(dim_data_pca$PC1 > 0, "F",
+                                 ifelse(dim_data_pca$PC1 < -1, "M", "")),
+                          dim_data_pca$sex)
+
+# Plot the results, show original data vs clustering data in different colors
+dim_data_pca$sex_origin = sex_data$sex_origin
+color_dict <- list(
+  red = "Female",
+  blue = "Male",
+  pink = "Female predicted",
+  lightblue = "Male predicted",
+  black = "Unknown sex")
+
+colors = ifelse(dim_data_pca$sex_origin == "Carly PCA",
+                ifelse(dim_data_pca$sex == "F", "red", "blue"),
+                ifelse(dim_data_pca$sex == "M", "lightblue",
+                       ifelse(dim_data_pca$sex == "F", "pink", "black")))
+plot(dim_data_pca$PC1, dim_data_pca$PC2, col=colors, pch=16, main="Sex clustering using PCA analysis")
+color_list = c("red", "pink", "blue", "lightblue", "black")
+color_meaning = sapply(color_list, function(x) color_dict[[x]])
+legend("topright", legend = color_meaning, fill = color_list)
+
+# Put sex data back into the original field data
+sex_prediction = subset(dim_data_pca, select = c("Bird", "sex"))
+field_data_without_sex = field_data[, !names(field_data) == "sex"]
+merged_field_data = merge(field_data_without_sex, sex_prediction, by = "Bird")
+
+# Write data back to csv file
+write.table(merged_field_data, file = "~/thesis/metadata/metadata.csv", sep = ",", row.names = FALSE, na = "")
