0403f39a4c92b3f70a0ec86c127f06bc60481e21
amyparsons <amyparsons@penguin>
2023-04-18 16:56:00 -0700

    made comments look pretty

--- R/decontam.R
+++ R/decontam.R
@@ -4,19 +4,19 @@ library(tidyverse)
 library(DT)
 library(qiime2R)
 
-#This file gives the code to decontaminate sequences using the fisher method. This method uses both 
-#frequency and prevelance to give each sequence a score. The default threshold is p = 0.01. 
-#Input: phylsoseq object that includes: dada2-table.qza, taxonomy.qza, metadata.tsv
-#Outputs: .tsv file that lists all of the contaminants and a .biom file of the decontaminated sOTU 
-#table. Import this file to the HPC and then use biom.slurm to convert this file to a .qza file for 
-#downstream analysis in qiime2.
+# This file gives the code to decontaminate sequences using the fisher method. This method uses both 
+# frequency and prevelance to give each sequence a score. The default threshold is p = 0.01. 
+# Input: phylsoseq object that includes: dada2-table.qza, taxonomy.qza, metadata.tsv
+# Outputs: .tsv file that lists all of the contaminants and a .biom file of the decontaminated sOTU 
+# table. Import this file to the HPC and then use biom.slurm to convert this file to a .qza file for 
+# downstream analysis in qiime2.
 
 
 # Use the line below to debug reading the metadata file
 # meta = read_q2metadata("~/thesis/metadata/metadata_phyloseq.tsv")
 
 
-#Create a phyloseq object
+# Create a phyloseq object
 ps = qza_to_phyloseq(
   features = "~/qiime/denoising/dada2-table.qza",
   # tree = "~/PhylogeneticTree/SILVAtree.qza",
@@ -24,7 +24,7 @@ ps = qza_to_phyloseq(
   metadata = "~/thesis/metadata/metadata_phyloseq.tsv")
 
 
-#Check the data
+# Check the data
 datatable(sample_data(ps))
 
 # Graph the distribution of samples and controls by library size
@@ -49,7 +49,7 @@ table(contamdf.freq$contaminant)
 head(which(contamdf.freq$contaminant))
 
 # Plots frequency against DNA concentration, dotted line shows non-contaminant distribution, red line shows contaminant 
-#distribution which would be inversely proportional to DNA concentration.
+# distribution which would be inversely proportional to DNA concentration.
 plot_frequency(ps, taxa_names(ps)[c(1,119)], conc="Concentration") + 
   xlab("DNA Concentration")
 
@@ -79,7 +79,7 @@ View(contamdf.prev.05)
 # result = FALSE: 20222 TRUE:1352
 
 # Prevelance and Freq. method = "both", threshold=0.5
-#Using method ="both" only identifies contaminants that are found by both prevelance and frequency methods
+# Using method ="both" only identifies contaminants that are found by both prevelance and frequency methods
 sample_data(ps)$is.neg = sample_data(ps)$Sample_or_control == "control"
 contamdf.both.05 = isContaminant(ps, method="both", conc = "Concentration", neg="is.neg")
 table(contamdf.both.05$contaminant)
@@ -99,8 +99,8 @@ View(contamdf.comb.05)
 #------------------------Decontam Run---------------------------------------
 
 # Prevelance and Freq. method = "combined", threshold=0.05
-#Using method ="combined" uses the fisher method to give a contamination score using both freq. and prev.
-#Used this method, since it was shown to be most robust in Davis et al.2018
+# Using method ="combined" uses the fisher method to give a contamination score using both freq. and prev.
+# Used this method, since it was shown to be most robust in Davis et al.2018
 sample_data(ps)$is.neg = sample_data(ps)$Sample_or_control == "control"
 contamdf.comb.01 = isContaminant(ps, method="combined", conc = "Concentration", neg="is.neg")
 table(contamdf.comb.01$contaminant)
@@ -119,10 +119,10 @@ View(tax2)
 contaminants = filter(tax2, contaminant == "TRUE")
 View(contaminants)
 
-#write the list of contaminants to a csv
+# Write the list of contaminants to a csv
 write.csv(contaminants, "~/thesis/metadata/contaminants_05.csv", row.names=FALSE)
 
-#Prune contaminant taxa out of OTU table
+# Prune contaminant taxa out of OTU table
 final_biom <- prune_taxa(!contamdf.comb.01$contaminant, ps)
 final_biom
 
@@ -130,9 +130,9 @@ final_biom
 
 library(biomformat);packageVersion("biomformat")
 
-#if taxa_are_rows=TRUE: otu = as(otu_table(globalpatterns),"matrix"))
-#if taxa_are_rows=FALSE: t(otu_table(globalpatterns),"matrix"))
-#Upload otu_biom.biom to HPC and run biom.slurm to convert into .qza file
+# If taxa_are_rows=TRUE: otu = as(otu_table(globalpatterns),"matrix"))
+# If taxa_are_rows=FALSE: t(otu_table(globalpatterns),"matrix"))
+# Upload otu_biom.biom to HPC and run biom.slurm to convert into .qza file
 
 otu = as(otu_table(final_biom),"matrix") 
 otu_biom = make_biom(data=otu)
